b61474e3a750dc0a7a3627b5ef5144de2ec521f6
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-07-07 13:54:55 +0530

    This commit for using props in header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React from 'react'
 import Header from "./Components/Header";
-
-// function App() {
-//   return (
-//     <div className="container">
-//       <Header/>
-//     </div>
-//   );
-// }
-
-class App extends React.Component{
-  render(){
-    return <h1>Hello from a class</h1>
-  }
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
